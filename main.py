effa8087ff6c2d268dcbe8d9a1f7965f06b6c685
Alan <22360@students.mrgs.school.nz>
2026-07-30 09:09:08 +1200

    copied the tkinter module from the tkinter documentation page as a start

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-test
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+ttk.Label(frm, text=".").grid(column=0, row=0)
+ttk.Button(frm, text="Exit", command=root.destroy).grid(column=1, row=0)
+
+root.mainloop()

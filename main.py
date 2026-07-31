97af7848e017628f79e3e48facc3bab2a78bbffe
Alan <22360@students.mrgs.school.nz>
2026-07-31 14:18:52 +1200

    added a class for how NZQA credits are calculated

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
+from collections import deque
+
+# a library for nz credits rank score
+class Subject:
+    def __init__(self, name, achieved, merit, excellence):
+        self.name = name
+        self.achieved = achieved
+        self.merit = merit
+        self.excellence = excellence
+
+    def calculate_score(self):
+
+        scores = []
+
+        scores += [4] * self.excellence
+        scores += [3] * self.merit
+        scores += [2] * self.achieved
+
+        scores.sort(reverse=True)
+
+        return sum(scores[:80])
+
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-
-ttk.Label(frm, text=".").grid(column=0, row=0)
-ttk.Button(frm, text="Exit", command=root.destroy).grid(column=1, row=0)
-
+app = RankScoreApp(root)
 root.mainloop()
